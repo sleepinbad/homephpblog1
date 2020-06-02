353312559004c19fc7cbeab62f9202e15389abbd
sleepinbad <54507222+sleepinbad@users.noreply.github.com>
2020-06-02 18:13:37 +0200

    Update db.php

--- homephpblog/application/config/db.php
+++ homephpblog/application/config/db.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'host' => 'localhost',
-	'name' => 'blog',
-	'user' => 'mysql',
-	'password' => 'mysql',
+	'host' => 'jfrpocyduwfg38kq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+	'name' => 'k38t5kw4hqmfbkoz',
+	'user' => 'yk5p15ou1bj3tct1',
+	'password' => 'ygcn3mzxuzf9q527',
 ];
